aeaf7d80507ceb442ea7f1cf9d01884d16eef2dc
Rafel460 <rafaelmonte46@outlook.com>
2019-11-07 22:15:39 -0200

    Versao 7/11

---
+++ src/app/aviso/aviso.ts
@@ -0,0 +1,8 @@
+import { Adm } from '../adm/adm';
+
+export class Aviso{
+    titulo : string;
+    mensagem : string;
+    cpf_receptor : string;
+    adm : Adm = new Adm();
+}
--- src/app/mensagem/mensagem.page.ts
+++ src/app/mensagem/mensagem.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {Aviso} from '../aviso/aviso';
+import { AngularFireDatabase} from '@angular/fire/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-mensagem',
@@ -6,10 +11,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mensagem.page.scss'],
 })
 export class MensagemPage implements OnInit {
+  aviso : Aviso = new Aviso();
 
-  constructor() { }
+  constructor(private banco : AngularFireDatabase, private autenticacao : AngularFireAuth, private warn : AlertController, private router : Router) { 
+    this.aviso.adm.nome = this.autenticacao.auth.currentUser.email;
+  }
 
   ngOnInit() {
+  }
+
+  async enviarMensagem(){
+    const alert = await this.warn.create({
+      header : 'Aviso',
+      message : 'Tem certeza que quer enviar?',
+      buttons : [{
+        text : 'Sim',
+        handler : () =>{
+          this.banco.list("Mensagem").push(this.aviso);
+          this.router.navigate(['destino']);
+        }
+      },{
+        text : 'Não'
+      }]
+      
+    });
+    await alert.present();
   }
 
 }
